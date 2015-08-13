8b9be0982dbd2609bf015296731af5ff26a940b2
Pawel Cendrzak <pabloxxl@fastmail.fm>
2015-08-13 21:30:26 +0200

    Added parsing commandline options
    Added useage of argparse.
    Added debug and cli/gtk option.

--- client.py
+++ client.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 
 from cli import window
+import argparse
 
-w = window()
-w.run()
-print "TERMINATED"
+VERSION = "0.1"
+
+# Parse program arguments
+parser = argparse.ArgumentParser(description="revi v"+VERSION,
+                                 epilog="developed by Pawel Cendrzak")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-g", "--gtk", action="store_true",
+                   help="use cli mode (ncurses)")
+group.add_argument("-c", "--cli", action="store_true",
+                   help="use graphical mode (GTK)")
+parser.add_argument("-d", "--debug",
+                    help="enter debug mode",
+                    action="store_true")
+
+args = parser.parse_args()
+
+# Run main loop
+if args.cli:
+    w = window()
+    w.run()
+    print "TERMINATED"
+else:
+    print "GTK version is not supported yet"
